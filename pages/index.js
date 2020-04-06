ce2418e7cb2455b2c360036ea95cd0bfedc9fe43
Bartłomiej Olech <olech.bartlomiej@gmail.com>
2020-04-06 20:32:46 +0200

    implement redux to app

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,34 @@ import React from "react";
 import StartPage from '../components/StartPage/StartPage';
 import styled from 'styled-components';
 
+import {connect} from 'react-redux';
+import {decrementCounter, incrementCounter} from '../redux/actions/counterActions';
+
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
 `
 
-const Main = () => {
+const Main = (props) => {
+
   return (
     <Container>
+                <button onClick={props.incrementCounter}>Increment</button>
+                <button onClick={props.decrementCounter}>Decrement</button>
+                <h1>{props.counter}</h1>
       <StartPage></StartPage>
     </Container>
   )
 }
 
-export default Main;
+const mapStateToProps = state => ({
+  counter: state.counter.value
+});
+
+const mapDispatchToProps = {
+  incrementCounter: incrementCounter,
+  decrementCounter: decrementCounter,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
