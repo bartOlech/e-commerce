b187ea591eb2e4a35198864ff5c0a0664206421d
Bartłomiej Olech <olech.bartlomiej@gmail.com>
2020-05-22 11:04:42 +0200

    implementation the logo to the project

--- components/Header/Logo.js
+++ components/Header/Logo.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FontStyle } from '../../assets/style/style';
+import Router from 'next/router'
 
 const Conatiner = styled.div`
-    width: 50px;
-    font-family: ${FontStyle.family};
-    font-size: 1.7em;
-    color: #3C3C3C;
-    padding-left: 15px;
+    width: 140px;
+    height: 70px;
     z-index: 2;
+    background-image: url('../../static/Logo/logo.svg');
+    background-repeat: no-repeat;
+    background-size: 140px 70px;
+    margin-top: -20px;
+    cursor: pointer;
+    @media (max-width: 1000px) {
+        width: 100px; 
+        background-size: 100px 70px;
+    }
 `
 
 const Logo = () => {
+
+    const goToHomePage = () => {
+        Router.push('/')
+    }
+
     return (
-        <Conatiner>
-            LOGO
+        <Conatiner onClick={goToHomePage}>
+            
         </Conatiner>
     )
 }
